47d6cc988b0fcb120d399ddbe4ba87b43676b058
geoffdudds <geoff.dudds@gmail.com>
2022-06-25 21:37:35 -0700

    basic schedule working with just printing stuff

--- chicken-gate.py
+++ chicken-gate.py
@@ -1,38 +1,85 @@
-# need to periodaically check time then raise or lower the gate
-
-
-from datetime import datetime
-from suntime import Sun, SunTimeException
+from datetime import datetime, date
+from suntime import Sun
 from dateutil import tz
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-# get sunrise, sunset
-latitude = 49.164379
-longitude = -123.936661
-sun = Sun(latitude, longitude)
+class ChickenGate:
+    def __init__(self):
+        self.sunrise = None
+        self.sunset = None
+        self.lift_job = None
+        self.lower_job = None
+
+        # create schedule, add job to update sunrise / sunset times, and start the scheduler
+        self.sched = BlockingScheduler()
+        self.update_sched_job = self.sched.add_job(
+            func=self.update_schedule,
+            trigger="cron",
+            hour=0,
+            minute=0,
+        )
+        self.update_schedule()
+        self.sched.start()
+
+    def update_schedule(self):
+        self.update_sunrise_sunset_times()
+        self.schedule_lift()
+        self.schedule_lower()
+
+        self.sched.print_jobs()
+
+    def update_sunrise_sunset_times(self):
+        latitude = 49.164379
+        longitude = -123.936661
+        sun = Sun(latitude, longitude)
+        from_zone = tz.gettz("UTC")
+        to_zone = tz.gettz()
+
+        # get sunrise time
+        sunrise_utc = sun.get_sunrise_time()
+        sunrise_utc = sunrise_utc.replace(tzinfo=from_zone)
+        self.sunrise = sunrise_utc.astimezone(to_zone)
+
+        # repeat for sunset time
+        sunset_utc = sun.get_sunset_time()
+        sunset_utc = sunset_utc.replace(tzinfo=from_zone)
+        self.sunset = sunset_utc.astimezone(to_zone)
+
+    def lift(self):
+        print("executing lift job...")
+
+    def lower(self):
+        print("executing lower job...")
+
+    def schedule_lift(self):
+        if self.lift_job is not None:
+            self.lift_job.remove()
+        self.lift_job = self.sched.add_job(
+            func=self.lift,
+            trigger="cron",
+            hour=self.sunset.hour,
+            minute=self.sunset.minute,
+        )
+
+        print("Gate schedule to lift at {}".format(self.sunset.strftime("%H:%M")))
+
+    def schedule_lower(self):
+        if self.lower_job is not None:
+            self.lower_job.remove()
+        self.lower_job = self.sched.add_job(
+            func=self.lower,
+            trigger="cron",
+            hour=self.sunrise.hour,
+            minute=self.sunrise.minute,
+        )
+
+        print("Gate schedule to lower at {}".format(self.sunrise.strftime("%H:%M")))
 
 
-# Get today's sunrise and sunset in UTC
-today_sr = sun.get_sunrise_time()
-today_ss = sun.get_sunset_time()
+def main():
+    chicken_gate = ChickenGate()
 
-from_zone = tz.gettz("UTC")
-to_zone = tz.gettz()
-today_sr = today_sr.replace(tzinfo=from_zone)
-local_sr = today_sr.astimezone(to_zone)
-today_ss = today_ss.replace(tzinfo=from_zone)
-local_ss = today_ss.astimezone(to_zone)
 
-print(
-    "Today at Nanaimo the sun raised at {} and get down at {} local time".format(
-        local_sr.strftime("%H:%M"), local_ss.strftime("%H:%M")
-    )
-)
-
-# get time
-now = datetime.now()
-
-print("Time in Nanimo is {}".format(now.strftime("%H:%M")))
-
-# if time is after between sunrise and senset, gate is open
-# otherwise, its closed
+if __name__ == "__main__":
+    main()
