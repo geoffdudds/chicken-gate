eae1e68143c7531ef0f78bf956b3df4ca6777396
Geoff <geoff.dudds@gmail.com>
2022-07-09 22:06:45 -0700

    wip setting up switch

--- gate.py
+++ gate.py
@@ -5,12 +5,10 @@ import time
 
 class Gate:
     def __init__(self):
-        print("setting up io's")
-        GPIO.setmode(GPIO.BCM)  # Broadcom pin-numbering scheme
-        GPIO.setup(4, GPIO.OUT)  # set Relay 1 output
+        self.switch = Button(4)   # gate closed switch (physical pin 7)
+        GPIO.setmode(GPIO.BCM)    # Broadcom pin-numbering scheme
+        GPIO.setup(4, GPIO.OUT)   # set Relay 1 output
         GPIO.setup(17, GPIO.OUT)  # set Relay 2 output
-
-        self.switch = Button(4)
 
     def lift(self):
         self.turn_cw()
@@ -23,14 +21,10 @@ class Gate:
         self.stop()
 
     def turn_cw(self):
-        # GPIO.setup(4, GPIO.OUT)  # set Relay 1 output
-        # GPIO.setup(17, GPIO.OUT)  # set Relay 2 output
         GPIO.output(4, GPIO.HIGH)
         GPIO.output(17, GPIO.LOW)
 
     def turn_ccw(self):
-        # GPIO.setup(4, GPIO.OUT)  # set Relay 1 output
-        # GPIO.setup(17, GPIO.OUT)  # set Relay 2 output
         GPIO.output(4, GPIO.LOW)
         GPIO.output(17, GPIO.HIGH)
 
