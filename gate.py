9370071a3bbe4e157a6daa056a9e7b02b15c7b95
geoffdudds <geoff.dudds@gmail.com>
2022-08-16 21:10:22 -0700

    move gate control to thread

--- gate.py
+++ gate.py
@@ -2,6 +2,7 @@ from typing import Callable
 import RPi.GPIO as GPIO
 from gpiozero import Button
 import time
+from threading import Thread
 
 
 class Gate:
@@ -11,21 +12,34 @@ class Gate:
         GPIO.setup(4, GPIO.OUT)  # set Relay 1 output
         GPIO.setup(17, GPIO.OUT)  # set Relay 2 output
         self.gate_position_fbk_cb = None
+        self.cmd = None
+        self.cmd_thread = Thread(target=self.process_inputs)
+        self.cmd_thread.start()
 
     def set_position_fbk_cbk(self, gate_position_fbk_cb):
         self.gate_position_fbk_cb = gate_position_fbk_cb
 
+    def process_inputs(self):
+        if self.cmd is "lift":
+            self.process_lift()
+        elif self.cmd is "lower":
+            self.process_lower()
+
     def lift(self):
+        self.cmd = "lift"
+
+    def lower(self):
+        self.cmd = "lower"
+
+    def process_lift(self):
         self.turn_cw()
         self.wait_until_raised()
         self.stop()
-        #self.run_position_fbk_cb(100)
 
-    def lower(self):
+    def process_lower(self):
         self.turn_ccw()
         self.wait_until_lowered()
         self.stop()
-        #self.run_position_fbk_cb(0)
 
     def turn_cw(self):
         GPIO.output(4, GPIO.HIGH)
@@ -48,7 +62,7 @@ class Gate:
         lower_time = 30
         for t in range(lower_time):
             time.sleep(1)
-            self.run_position_fbk_cb(((lower_time - t) / lower_time)*100)
+            self.run_position_fbk_cb(((lower_time - t) / lower_time) * 100)
 
     def run_position_fbk_cb(self, position):
         cb = self.gate_position_fbk_cb
