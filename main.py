f7ef69f940aa9d42f59d6594a50e56cda33b7ece
geoffdudds <geoff.dudds@gmail.com>
2022-09-19 21:10:28 -0700

    wip

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import time
+
+time.sleep(60)
+
 from gate import Gate
 from schedule import Schedule
 from api import Api
 import errno
 
-# import time
 
 # from signal import signal, SIGPIPE, SIG_DFL
 # signal(SIGPIPE,SIG_DFL)
 
 
 def main():
+
     gate = Gate()
     api = Api(gate)
     schedule = Schedule(gate)
