fabc5b600202ecdfb826604fcede2d7542f22c24
geoffdudds <geoff.dudds@gmail.com>
2022-11-01 21:27:50 -0700

    another try to reboot

--- schedule.py
+++ schedule.py
@@ -39,7 +39,8 @@ class Schedule:
 
     def update_schedule_job(self):
         # force program to restart. Required for blynk to keep working
-        os.system("systemctl restart chickengate.service")
+        # os.system("systemctl restart chickengate.service")
+        os.system("reboot")
         # sys.exit(3)
         self.update_schedule()
 
