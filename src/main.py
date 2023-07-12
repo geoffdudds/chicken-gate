1c12d71a946b31811b3489a9a10d05687e3fdbae
geoffdudds <geoff.dudds@gmail.com>
2023-07-12 09:15:50 -0700

    only put api stuff in try/except statement

--- src/main.py
+++ src/main.py
@@ -34,6 +34,8 @@ def elapse_100ms():
 
 def main():
     global tick_100ms
+    pipe_error = False
+    other_error = False
     gate = Gate()
     gate_drv = Gate_drv(gate)
     api = Api()
@@ -46,42 +48,44 @@ def main():
         try:
             api.run()
 
-            while tick_100ms > 0:
-                tick_100ms -= 1
-                
-                # push api commands to driver
-                gate_drv.reset_posn_to(api.get_posn_reset())
-                api_cmd = api.get_cmd()
-                if api_cmd == Cmd.OPEN:
-                    print("app cmd to open gate")
-                    gate_drv.open()
-                elif api_cmd == Cmd.CLOSE:
-                    print("app cmd to close gate")
-                    gate_drv.close()
-
-                # push api commands to driver
-                sched_cmd = schedule.get_gate_cmd()
-                if sched_cmd == Cmd.OPEN:
-                    print("sched cmd to open gate")
-                    gate_drv.open()
-                elif sched_cmd == Cmd.CLOSE:
-                    print("sched cmd to close gate")
-                    gate_drv.close()
-
-                gate_drv.tick()
-                api.set_posn(gate_drv.get_posn())
-
         except IOError as e:
             if e.errno == errno.EPIPE:
-                print("Caught pipe error - restarting...")
-                send_email("Pipe error")
+                print("Caught pipe error")
+                if pipe_error is False:
+                    send_email("Pipe error")
+                    pipe_error = True
                 print(e)
             else:
-                print("Caught unhandled exception - not restarting")
-                send_email("Some other error")
+                print("Caught unhandled exception")
+                if other_error is False:
+                    send_email("Some other error")
+                    other_error = True
                 print(e)
-            os.system("/usr/bin/bash reboot")
 
+        while tick_100ms > 0:
+            tick_100ms -= 1
+            
+            # push api commands to driver
+            gate_drv.reset_posn_to(api.get_posn_reset())
+            api_cmd = api.get_cmd()
+            if api_cmd == Cmd.OPEN:
+                print("app cmd to open gate")
+                gate_drv.open()
+            elif api_cmd == Cmd.CLOSE:
+                print("app cmd to close gate")
+                gate_drv.close()
+
+            # push api commands to driver
+            sched_cmd = schedule.get_gate_cmd()
+            if sched_cmd == Cmd.OPEN:
+                print("sched cmd to open gate")
+                gate_drv.open()
+            elif sched_cmd == Cmd.CLOSE:
+                print("sched cmd to close gate")
+                gate_drv.close()
+
+            gate_drv.tick()
+            api.set_posn(gate_drv.get_posn())
 
 if __name__ == "__main__":
     main()
